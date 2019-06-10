bd4d75be1687443cdb59363346edf10a77d84fa4
bouzalmat <abderrahman510@gmail.com>
2019-06-10 03:44:12 +0100

    Connect to broker

--- app/src/main/java/com/example/arduinoremotecontrol/MainActivity.java
+++ app/src/main/java/com/example/arduinoremotecontrol/MainActivity.java
@@ -5,8 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.arduinoremotecontrol.utils.GlobalVars;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class MainActivity extends AppCompatActivity {
+    MqttAndroidClient client;
     Button manual_btn;
     Button auto_btn;
     @Override
@@ -16,6 +27,34 @@ public class MainActivity extends AppCompatActivity {
 
         manual_btn = (Button) findViewById(R.id.manual_btn);
         auto_btn = (Button) findViewById(R.id.auto_btn);
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), GlobalVars.URI, clientId);
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+        options.setCleanSession(false);
+        options.setUserName(GlobalVars.USERNAME);
+        options.setPassword(GlobalVars.PASSWORD.toCharArray());
+        try {
+            IMqttToken token = client.connect(options);
+            //IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
 
         manual_btn.setOnClickListener(new View.OnClickListener() {
             @Override
