b137679da2668c5fbbbbb38af9b43385c72d50f6
bouzalmat <abderrahman510@gmail.com>
2019-06-10 03:19:24 +0100

    Add global variables

---
+++ app/src/main/java/com/example/arduinoremotecontrol/utils/GlobalVars.java
@@ -0,0 +1,10 @@
+package com.example.arduinoremotecontrol.utils;
+
+/**
+ * Created by BouzalmatAbderrahman on 6/10/2019
+ */
+public class GlobalVars {
+    public static String URI = "tcp://m24.cloudmqtt.com:10694";
+    public static String USERNAME = "uswtqssy";
+    public static String PASSWORD = "FXkDxJ6E4tmm";
+}
